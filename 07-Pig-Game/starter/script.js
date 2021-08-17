d52a9a31a92921df3afa780e0292c57e334416dc
brigo <brigovaczbalazs@hotmail.com>
2021-08-17 21:12:07 +0200

    Change player

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 //selecting elements
+const player0 = document.querySelector('.player--0');
+const player1 = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -16,18 +18,31 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 btnRoll.addEventListener('click', function () {
   const dice = Math.trunc(Math.random() * 6) + 1;
-  console.log(dice);
 
   diceEl.classList.remove('hidden');
   diceEl.src = `dice-${dice}.png`;
 
   if (dice !== 1) {
     currentScore += dice;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    console.log('[adding]');
+    console.log('   ' + activePlayer);
+    console.log('   ' + currentScore);
   } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0.classList.toggle('player--active');
+    player1.classList.toggle('player--active');
+    console.log('[changePlayer]');
+    console.log('   ' + activePlayer);
+    console.log('   ' + currentScore);
   }
 });
