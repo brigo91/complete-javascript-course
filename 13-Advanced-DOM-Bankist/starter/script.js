37b1e7a3c37b5a8f5051442df4f11b354bb97652
brigo <brigovaczbalazs@hotmail.com>
2022-03-29 18:51:55 +0200

    Building a tabbed component

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -84,6 +84,29 @@ document.querySelector('.nav__links').addEventListener('click', (e) => {
   }
 });
 
+//Tabbed component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsConent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', (e) => {
+  const clicked = e.target.closest('.operations__tab');
+
+  //Guard clause
+  if(!clicked) return;
+
+  //Remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsConent.forEach(c => c.classList.remove('operations__content--active'));
+
+  clicked.classList.add('operations__tab--active');
+
+  //Active content area
+  document.querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+  
+});
+
 /////////////////////////////
 /////////////////////////////
 /////////////////////////////
