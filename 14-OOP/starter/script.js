b48466206e8156efaaebf2c5f07b66ec839fb7c5
brigo <brigovaczbalazs@hotmail.com>
2022-03-31 16:50:43 +0200

    Prototypes

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -17,3 +17,21 @@ const matilda = new Person('Matilda', 2017);
 const jack = new Person('jack', 1975);
 
 console.log(jonas instanceof Person);
+
+//Prototypes
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function(){
+	console.log(2036 - this.birthYear);
+};
+
+jonas.calcAge();
+matilda.calcAge();
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(matilda));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+Person.prototype.pieces = 'Homo sapiens';
